e7c2d86ae36268e8bd4b4c7de56bde1874ca5340
Joonas Suurväli <joonas.suurvali@solita.fi>
2021-02-02 15:31:11 +0200

    renamed some variables

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ canvas.height = document.querySelector('img').clientHeight;
 canvas.width = document.querySelector('img').clientWidth;
 const clearCanvasBtn = document.querySelector('#clear-canvas');
 const ctx = canvas.getContext('2d');
-let down = false;
+let isMouseDown = false;
 let startX = 0;
 let startY = 0;
-let width;
-let height;
+let rectWidth;
+let rectHeight;
 const rectangles = [];
 
 canvas.addEventListener('mousedown', (event) => {
-  down = true;
+  isMouseDown = true;
   startX = event.offsetX;
   startY = event.offsetY;
 });
 canvas.addEventListener('mouseup', () => {
-  down = false;
+  isMouseDown = false;
   rectangles.push({
     startX,
     startY,
-    width,
-    height,
+    rectWidth,
+    rectHeight,
   });
 });
 clearCanvasBtn.addEventListener('click', () => {
@@ -31,30 +31,30 @@ clearCanvasBtn.addEventListener('click', () => {
   rectangles.length = 0;
 });
 
-const drawRectangle = (startX, startY, width, height) => {
+const drawRectangle = (startX, startY, rectWidth, rectHeight) => {
   ctx.strokeStyle = '#000';
   ctx.lineWidth = 5;
   ctx.beginPath();
-  ctx.rect(startX, startY, width, height);
+  ctx.rect(startX, startY, rectWidth, rectHeight);
   ctx.stroke();
 };
 
-const handleCoordinates = (mouseX, mouseY) => {
-  width = mouseX - startX;
-  height = mouseY - startY;
-  if (down) {
+const calculateCoordinates = (mouseX, mouseY) => {
+  rectWidth = mouseX - startX;
+  rectHeight = mouseY - startY;
+  if (isMouseDown) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (rectangles.length > 0) {
-      rectangles.forEach(({ startX, startY, width, height }) => {
-        drawRectangle(startX, startY, width, height);
+      rectangles.forEach(({ startX, startY, rectWidth, rectHeight }) => {
+        drawRectangle(startX, startY, rectWidth, rectHeight);
       });
     }
-    drawRectangle(startX, startY, width, height);
+    drawRectangle(startX, startY, rectWidth, rectHeight);
   }
 };
 
 canvas.addEventListener('mousemove', (event) => {
   const mouseX = event.offsetX - rect.left;
   const mouseY = event.offsetY - rect.top;
-  handleCoordinates(mouseX, mouseY);
+  calculateCoordinates(mouseX, mouseY);
 });
