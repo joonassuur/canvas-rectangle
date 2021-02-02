dc4f1623faa21926eb1f476e33855ab227ff2ea9
Joonas Suurväli <joonas.suurvali@solita.fi>
2021-02-02 15:19:14 +0200

    added ability to draw multiple rectangles and clear canvas feature

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const canvas = document.querySelector('canvas');
 const rect = canvas.getBoundingClientRect();
 canvas.height = document.querySelector('img').clientHeight;
 canvas.width = document.querySelector('img').clientWidth;
-
+const clearCanvasBtn = document.querySelector('#clear-canvas');
 const ctx = canvas.getContext('2d');
 let down = false;
 let startX = 0;
 let startY = 0;
+let width;
+let height;
+const rectangles = [];
 
 canvas.addEventListener('mousedown', (event) => {
   down = true;
@@ -15,23 +18,43 @@ canvas.addEventListener('mousedown', (event) => {
 });
 canvas.addEventListener('mouseup', () => {
   down = false;
+  rectangles.push({
+    startX,
+    startY,
+    width,
+    height,
+  });
+});
+clearCanvasBtn.addEventListener('click', () => {
+  // clear rectangles
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  rectangles.length = 0;
 });
 
-const draw = (mouseX, mouseY) => {
-  const width = mouseX - startX;
-  const height = mouseY - startY;
+const drawRectangle = (startX, startY, width, height) => {
+  ctx.strokeStyle = '#000';
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.rect(startX, startY, width, height);
+  ctx.stroke();
+};
+
+const handleCoordinates = (mouseX, mouseY) => {
+  width = mouseX - startX;
+  height = mouseY - startY;
   if (down) {
-    ctx.strokeStyle = '#000';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.rect(startX, startY, width, height);
-    ctx.lineWidth = 5;
-    ctx.stroke();
+    if (rectangles.length > 0) {
+      rectangles.forEach(({ startX, startY, width, height }) => {
+        drawRectangle(startX, startY, width, height);
+      });
+    }
+    drawRectangle(startX, startY, width, height);
   }
 };
 
 canvas.addEventListener('mousemove', (event) => {
   const mouseX = event.offsetX - rect.left;
   const mouseY = event.offsetY - rect.top;
-  draw(mouseX, mouseY);
+  handleCoordinates(mouseX, mouseY);
 });
